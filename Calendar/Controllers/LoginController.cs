fcf9fe44b61801d46c64853051bcabe58ceabd1b
Adel-Atzouza <1032542@hr.nl>
2024-11-05 23:43:00 +0100

    Admin Login

--- Calendar/Controllers/LoginController.cs
+++ Calendar/Controllers/LoginController.cs
@@ -19,20 +19,45 @@ public class LoginController : Controller
     [HttpPost("Login")]
     public IActionResult Login([FromBody] LoginBody loginBody)
     {
-        // TODO: Impelement login method
-        return Unauthorized("Incorrect password");
+        // TODO: Implement login method
+        if (loginBody == null || string.IsNullOrEmpty(loginBody.Username) || string.IsNullOrEmpty(loginBody.Password))
+        {
+            return BadRequest("Username or password is missing");
+        }
+
+        var loginStatus = _loginService.CheckPassword(loginBody.Username, loginBody.Password);
+        if (loginStatus == LoginStatus.Success)
+        {
+            HttpContext.Session.SetString(ADMIN_SESSION_KEY.adminLoggedIn.ToString(), loginBody.Username);
+            return Ok("Login successful");
+        }
+
+        return loginStatus == LoginStatus.IncorrectUsername
+            ? Unauthorized("Incorrect username")
+            : Unauthorized("Incorrect password");
     }
+
 
     [HttpGet("IsAdminLoggedIn")]
     public IActionResult IsAdminLoggedIn()
     {
         // TODO: This method should return a status 200 OK when logged in, else 403, unauthorized
+        var adminLoggedIn = HttpContext.Session.GetString(ADMIN_SESSION_KEY.adminLoggedIn.ToString());
+        if (adminLoggedIn != null)
+        {
+            return Ok(adminLoggedIn);
+        }
         return Unauthorized("You are not logged in");
     }
 
     [HttpGet("Logout")]
     public IActionResult Logout()
     {
+        if (HttpContext.Session.GetString(ADMIN_SESSION_KEY.adminLoggedIn.ToString()) == null)
+        {
+            return Unauthorized("You are not logged in");
+        }
+        HttpContext.Session.Remove(ADMIN_SESSION_KEY.adminLoggedIn.ToString());
         return Ok("Logged out");
     }
 
